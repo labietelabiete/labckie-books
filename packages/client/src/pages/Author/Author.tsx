786edef5f577e9a61aeaeb8a8c00572960b989ca
labietelabiete <victorhugogote@gmail.com>
2021-12-01 10:55:00 +0100

    Feat: react router dom set up

--- packages/client/src/pages/Author/Author.tsx
+++ packages/client/src/pages/Author/Author.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Author() {
-  return <div></div>;
+  return <div>Author</div>;
 }
--- packages/client/src/pages/Book/Book.tsx
+++ packages/client/src/pages/Book/Book.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Book() {
-  return <div></div>;
+  return <div>Book</div>;
 }
